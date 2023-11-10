2ab38cd24889aded4e71c02872e1d27aaa29e0ee
samantha gard <samantha.gard13@gmail.com>
2023-11-10 09:09:44 -0500

    heroku push

--- config/connection.js
+++ config/connection.js
@@ -4,20 +4,24 @@ require("dotenv").config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
+      host: "localhost",
       dialect: "mysql",
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      },
+      port: 3306,
     }
   );
 }
