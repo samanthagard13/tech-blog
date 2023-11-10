bf669a9ce04f6815607a4e91442f7ba21846105b
samantha gard <samantha.gard13@gmail.com>
2023-11-10 09:06:57 -0500

    heroku push

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-let sequelize; 
+let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
@@ -11,12 +11,15 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: "localhost",
       dialect: "mysql",
-      port: 3306,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
     }
   );
 }
 
 module.exports = sequelize;
-
